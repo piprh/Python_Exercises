daa8025889ccec3ca41597119e656602880edaf2
Pip Hill <s2691680@bioinfmsc8.bio.ed.ac.uk>
2024-11-01 11:07:04 +0000

    updates to Exercises in LabPy03

--- LabPy03/Exercise1.py
+++ LabPy03/Exercise1.py
@@ -12,9 +12,33 @@ DNA_file = open("DNA.txt",'w')
 for eachline in lines:
     DNAnum +=1
     DNAid = "seq" + str(DNAnum)
-    line_trim = eachline[15:]
+    line_trim = eachline[14:]
     print(DNAid, "is ", len(line_trim.rstrip()),"bases long.")
+    print(line_trim.rstrip())
     DNA_file.write(f"{DNAid}: {line_trim}")
 #Close files
 input_file.close()
 DNA_file.close()
+
+#Open and read genomic file
+exons = open('exons.txt').read().rstrip()
+counter=0
+for exon in exons:
+    counter+=1
+    print(counter, exon)
+with
+open('Exercise2_coding_sequence.fasta','w') as fullcoding:
+    fullcoding.write('>Lecture13_exercise2_codingseq\n')
+    for exon in exons:
+        counter +=1
+        startex = int(exon.split(',')[0])-1
+        endex = int(exon.split(',')[1])
+        exwanted = genomic_dna2[startex : endex]
+        fullcoding.write(exwanted)
+        regionsum = 'Exon'+str(counter)+' '+str(exons)+' runs from index position '+str(startexon)+' upto but not including '+str(endexon)+ '.'
+        print(regionsum,'\n\t',exwanted)
+
+##SLIDING WINDOWS
+print('Starting sliding window')
+import os, shutil
+
